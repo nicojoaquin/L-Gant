b53576ac80c65422bafb982c0977b5443142cd95
nicojoaquin <nicojoaquin1998@gmail.com>
2021-11-25 00:43:34 -0300

    useP

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useContext } from "react";
+import { useEffect, useState, useRef } from "react";
 import { collection, getDocs } from "@firebase/firestore";
 import db from "../config/firebase-config";
 
